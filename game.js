c9d4533de067ed6229d49b4a054c63c4c8abca59
MelakuDemeke <melaku.demeke789@gmail.com>
2022-07-09 22:24:21 +0300

    repeted code refactored into a function

--- game.js
+++ game.js
@@ -8,12 +8,18 @@ function nextSequence() {
     gamePattern.push(randomChosenColur);
     $("#"+randomChosenColur).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/"+randomChosenColur+".mp3");
-    audio.play();
+    playSound(randomChosenColur);
 }
 
 $(".btn").click(function (e) { 
     var userChosenColour = this.id;
     // console.log(userChosenColour);
     userClickPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
 });
+
+function playSound(name) {
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
