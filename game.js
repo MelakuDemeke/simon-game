ae4ebc3aad0cb2386a310ca4d96a96e03ef6b32f
MelakuDemeke <melaku.demeke789@gmail.com>
2022-07-09 23:01:50 +0300

    leven counter and key press detect added

--- game.js
+++ game.js
@@ -1,15 +1,29 @@
 var buttonColurs = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userClickPattern = [];
+var gameStarted = false;
+var level = 0;
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     randomChosenColur = buttonColurs[randomNumber];
     gamePattern.push(randomChosenColur);
     $("#"+randomChosenColur).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    level++;
+    $("#level-title").text("Level "+ level);
 
     playSound(randomChosenColur);
+    console.log("started");
 }
+
+$(document).keypress(function (e) { 
+    if(gameStarted == false){
+        nextSequence();
+        gameStarted = true;
+
+    }   
+});
 
 $(".btn").click(function (e) { 
     var userChosenColour = this.id;
